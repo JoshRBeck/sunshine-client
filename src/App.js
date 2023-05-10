28e2f04855446d9b779e50f6a3c517713e6c0b3d
beckjosh <Joshbeck17@gmail.com>
2023-05-10 15:09:30 +0200

    More Auth Fun

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar"
 import { Routes, Route } from "react-router-dom"
 import HomePage from './pages/HomePage';
 import ProfilePage from "./pages/ProfilePage"
-import SignupPage from "./page/SignupPage"
-import LoginPage from "./pages/SignupPage"
+import SignupPage from "./pages/SignupPage"
+import LoginPage from "./pages/LoginPage"
 import WineListPage from "./pages/WineListPage"
 import WineListDetailsPage from "./pages/WineListDetailsPage"
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-
+      <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/profile" element={<ProfilePage/>} />
-        <Route path="/signup" element={<SignupPage/>} />
-        <Route path="/login" elment={<LoginPage/>} />
-        <Route path="/wine-list" element={<WineListPage/>} />
-        <Route path="/wine-list-details" element={<WineListDetailsPage/>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<isPrivate><ProfilePage /></isPrivate>} />
+        <Route path="/signup" element={<isAnon><SignupPage /></isAnon>} />
+        <Route path="/login" elment={<isAnon><LoginPage /></isAnon>} />
+        <Route path="/wine-list" element={<isAnon><WineListPage /></isAnon>} />
+        <Route path="/wine-list-details" element={<isAnon><WineListDetailsPage /></isAnon>} />
       </Routes>
     </div>
   );
---
+++ src/context/auth.context.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { response } from "../../../sunshine-server/app";
+const API_URL = "http://localhost:5005";
+
+const AuthContext = React.createContext();
+
+function AuthProviderWrapper(props) {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isloading, setLoading] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const storeToken = (token) => {
+    localStorage.setItem('authtoken', token);
+  }
+  const authenticateUser = () => {
+    //Get the stored token from the local storage
+    const storedToken = localStorage.getItem('authToken');
+
+    //If the stored token exists in the local storage
+    if (storedToken) {
+      //We must send the JWT token in the requests "Authorization" headers
+      axios.get(
+        `${API_URL}/auth/verify`,
+        { headers: { Authorization: `Bearer: ${storedToken}` } }
+      )
+        .then((response) => {
+          // If the server verifies that the JWT token is valid
+          const user = response.data;
+          // Update store variables
+          setLoggedIn(true);
+          setLoading(true);
+          setUser(User);
+        })
+    } else {
+      //If the token is not available or removed
+      setLoggedIn(false);
+      setLoading(false);
+      setUser(null);
+    }
+  }
+  const removeToken = () => {
+    //Upon logout remove token from local storage
+    localStorage.removeItem("authToken");
+  }
+
+  const logOutUser = () => {
+    // To log out the user, remove the token
+    removeToken();
+    // and update the state variables    
+    authenticateUser();
+  }
+
+  useEffect(() => {
+    authenticateUser();
+  }, [])
+
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, isloading, user, storeToken, authenticateUser, logOutUser }}>
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
+
+export { AuthContext, AuthProviderWrapper };
