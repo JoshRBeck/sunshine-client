076a5dafe939aa9a84655f698a6123fcceb5f127
Lukas Jülich <lukas-juelich@web.de>
2023-05-10 16:07:16 +0200

    app.js changed

--- src/App.js
+++ src/App.js
@@ -14,21 +14,12 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
-<<<<<<< HEAD
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/profile" element={<ProfilePage/>} />
-        <Route path="/signup" element={<SignupPage/>} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/wine-list" element={<WineListPage/>} />
-        <Route path="/wine-list-details" element={<WineListDetailsPage/>} />
-=======
         <Route path="/" element={<HomePage />} />
         <Route path="/profile" element={<isPrivate><ProfilePage /></isPrivate>} />
         <Route path="/signup" element={<isAnon><SignupPage /></isAnon>} />
         <Route path="/login" elment={<isAnon><LoginPage /></isAnon>} />
         <Route path="/wine-list" element={<isAnon><WineListPage /></isAnon>} />
         <Route path="/wine-list-details" element={<isAnon><WineListDetailsPage /></isAnon>} />
->>>>>>> 28e2f04855446d9b779e50f6a3c517713e6c0b3d
       </Routes>
     </div>
   );
