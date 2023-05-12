e6c5c1d3475b6f5909f5bf16afd423b8deb25760
Lukas Jülich <lukas-juelich@web.de>
2023-05-12 11:42:46 +0200

    deleted tesseract

--- src/pages/WineListPage.js
+++ src/pages/WineListPage.js
@@ -2,13 +2,16 @@ import WineCard1 from "../assets/wine-card1.png";
 import WineCard2 from "../assets/wine-card2.png";
 import WineCard3 from "../assets/wine-card3.png";
 import WineCard4 from "../assets/wine-card4.png";
-import BackButton from "../assets/BackButton.png"
+import BackButton from "../assets/BackButton.png";
 
 function WineListPage() {
+
   return (
     <div className="WineListCard_Container">
       <div className="Heading_Container">
-      <a href="/profile"><img src={BackButton} alt="GoBack"/></a>
+        <a href="/profile">
+          <img src={BackButton} alt="GoBack" />
+        </a>
         <h2>SUNSHINE</h2>
       </div>
       <div clasName="WineListCard">
