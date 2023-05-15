1b70bd60a3b2e4f049b5d59719500a056cd0edfc
Lukas Jülich <lukas-juelich@web.de>
2023-05-15 15:09:18 +0200

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import './App.css';
 import Navbar from "./components/Navbar"
 import { Routes, Route } from "react-router-dom"
+import { useState, useEffect } from "react";
 import HomePage from './pages/HomePage';
 import ProfilePage from "./pages/ProfilePage"
 import SignupPage from "./pages/SignupPage"
 import LoginPage from "./pages/LoginPage"
 import WineListPage from "./pages/WineListPage"
 import WineListDetailsPage from "./pages/WineListDetailsPage"
+import ErrorPage from './pages/ErrorPage';
 import IsPrivate from './components/IsPrivate';
 import IsAnon from './components/IsAnon';
 import AddWine from './pages/AddWine';
 import winesData from "./wines-data.json"
+import axios from "axios"
 
 
 function App() {
+  const [setWines] = useState(null)
+
+  const API_URL = "http://localhost:5005";
+
+  const getAllWines = () => {
+    const storedToken = localStorage.getItem("authToken");
+
+    axios
+      .get(`${API_URL}/api/wines`, { headers: { Authorization: `Bearer ${storedToken}` } })
+      .then((response) => setWines(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getAllWines();
+  }, [] );
+
   return (
     <div className="App">
       <Navbar />
@@ -22,9 +42,10 @@ function App() {
         <Route path="/signup" element={<IsAnon><SignupPage /></IsAnon>} />
         <Route path="/login" element={<IsAnon><LoginPage /></IsAnon>} />
         <Route path="/wine-list" element={<IsAnon><WineListPage wines={winesData} /></IsAnon>} />
-        <Route path="/wine-list-details" element={<IsAnon><WineListDetailsPage wines={winesData} /></IsAnon>} />
+        <Route path="/wine-list/:wineId" element={<IsAnon><WineListDetailsPage wines={winesData} allWines={getAllWines} /></IsAnon>} />
         <Route path="/profile" element={<IsPrivate><ProfilePage /></IsPrivate>} />
         <Route path="/addwine" element={<AddWine />} />
+        <Route path="*" element={<ErrorPage/>}/>
       </Routes>
     </div>
   );
--- src/pages/WineListDetailsPage.js
+++ src/pages/WineListDetailsPage.js
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react"
+import { useParams, Link } from "react-router-dom"
+
 function WineListDetails(props) {
+    const [foundWine, setFoundWine] = useState(null)
+
+    const { wineId } = useParams()
+
+    useEffect(() => {
+        const wine = props.wines.find(wineObj => {
+          return wineObj._id === wineId
+        })
+    
+        if (wine) {
+          setFoundWine(wine)
+        }
+      }, [wineId])
+
     return (
         <div class="WineList_Details">
-            <h1>Wine Details Overview</h1>
+            <h1>Wine Details</h1>
+            {!foundWine && <h3>Wine not found!</h3>}
+
+            {foundWine && (
+        <>
+          <h2>{foundWine.name}</h2>
+          <h3>Type: {foundWine.type}</h3>
+          <p>{foundWine.variety}</p>
+          <p>{foundWine.region}</p>
+          <Link to="/wine-list">Back</Link>
+        </>
+      )}
 
         </div>
     )
