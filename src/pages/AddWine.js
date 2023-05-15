e331f22cc4e2c4aeac583d6648cf591f889f0891
beckjosh <Joshbeck17@gmail.com>
2023-05-15 10:44:26 +0200

    Co-authored-by: Marco Lölkes <mloelkes@users.noreply.github.com>

--- src/pages/AddWine.js
+++ src/pages/AddWine.js
@@ -63,99 +63,115 @@ function AddWine(props) {
     })
 
   return (
-    <div>
-      <h1>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor='name'>Name:</label>
-          <input
-            type="text"
-            name="name"
-            value={wine.name}
-            onChange={(e) => setWine.name(e.target.value)}>
-          </input>
-          <label htmlFor='type'>Type:</label>
-          <input
-            type="text"
-            name="type"
-            value={wine.type}
-            onChange={(e) => setWine.type(e.target.value)}>
-          </input>
-          <h3>Attributes:</h3>
-          <label htmlFor='acidity'>Acidity:</label>
-          <input
-            type="text"
-            name="type"
-            value={wine.attributes.acidity}
-            onChange={(e) => setWine.attributes.acidity(e.target.value)}>
-          </input>
-          <label htmlFor='sweetness'>Sweetness:</label>
-          <input
-            type="text"
-            name="type"
-            value={wine.attributes.sweetness}
-            onChange={(e) => setWine.attributes.sweetness(e.target.value)}>
-          </input>
-          <label htmlFor='alcohol'>Alcohol:</label>
-          <input
-            type="text"
-            name="type"
-            value={wine.attributes.alcohol}
-            onChange={(e) => setWine.attributes.alcohol(e.target.value)}>
-          </input>
-          <label htmlFor='age'>Age:</label>
-          <input
-            type="text"
-            name="type"
-            value={wine.attributes.age}
-            onChange={(e) => setWine.attributes.age(e.target.value)}>
-          </input>
-          <label htmlFor='structure'>Structure:</label>
-          <input
-            type="text"
-            name="type"
-            value={wine.attributes.structure}
-            onChange={(e) => setWine.structure(e.target.value)}>
-          </input>
-          <label htmlFor='finish'>Finish:</label>
-          <input
-            type="text"
-            name="type"
-            value={wine.attributes.finish}
-            onChange={(e) => setWine.attributes.finish(e.target.value)}>
-          </input>
-          <label htmlFor='tannins'>Tannins:</label>
-          <input
-            type="text"
-            name="type"
-            value={wine.attributes.tannins}
-            onChange={(e) => setWine.attributes.tannins(e.target.value)}>
-          </input>
-          <label htmlFor='fruitlevel'>Fruit Level:</label>
-          <input
-            type="text"
-            name="type"
-            value={wine.attributes.fruitLevel}
-            onChange={(e) => setWine.attributes.fruitLevel(e.target.value)}>
-          </input>
-          <br>
-          </br>
-          <label htmlFor='variety'>Variety:</label>
-          <input
-            type="text"
-            name="type"
-            value={wine.variety}
-            onChange={(e) => setWine.variety(e.target.value)}>
-          </input>
-          <label htmlFor='age'>Age:</label>
-          <input
-            type="text"
-            name="type"
-            value={wine.region}
-            onChange={(e) => setWine.region(e.target.value)}>
-          </input>
-          <button type="submit">Add Wine</button>
-        </form>
-      </h1>
+    <div className="container">
+      <div className="row">
+        <div className="wine-form col-12">
+          <h1>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor='name' className='addWineLabel'>Name:</label>
+              <input
+                type="text"
+                name="name"
+                className="addWineInput"
+                value={wine.name}
+                onChange={(e) => setWine.name(e.target.value)}>
+              </input>
+              <label htmlFor='type' className='addWineLabel'>Type:</label>
+              <input
+                type="text"
+                name="type"
+                className="addWineInput"
+                value={wine.type}
+                onChange={(e) => setWine.type(e.target.value)}>
+              </input>
+              <h2 className='attributesLabel'>Attributes:</h2>
+              <label htmlFor='acidity' className='addWineLabel'>Acidity:</label>
+              <input
+                type="text"
+                name="type"
+                className="addWineInput"
+                value={wine.attributes.acidity}
+                onChange={(e) => setWine.attributes.acidity(e.target.value)}>
+              </input>
+              <label htmlFor='sweetness' className='addWineLabel'>Sweetness:</label>
+              <input
+                type="text"
+                name="type"
+                className="addWineInput"
+                value={wine.attributes.sweetness}
+                onChange={(e) => setWine.attributes.sweetness(e.target.value)}>
+              </input>
+              <label htmlFor='alcohol' className='addWineLabel'>Alcohol:</label>
+              <input
+                type="text"
+                name="type"
+                className="addWineInput"
+                value={wine.attributes.alcohol}
+                onChange={(e) => setWine.attributes.alcohol(e.target.value)}>
+              </input>
+              <label htmlFor='age' className='addWineLabel'>Age:</label>
+              <input
+                type="text"
+                name="type"
+                className="addWineInput"
+                value={wine.attributes.age}
+                onChange={(e) => setWine.attributes.age(e.target.value)}>
+              </input>
+              <label htmlFor='structure' className='addWineLabel'>Structure:</label>
+              <input
+                type="text"
+                name="type"
+                className="addWineInput"
+                value={wine.attributes.structure}
+                onChange={(e) => setWine.structure(e.target.value)}>
+              </input>
+              <label htmlFor='finish' className='addWineLabel'>Finish:</label>
+              <input
+                type="text"
+                name="type"
+                className="addWineInput"
+                value={wine.attributes.finish}
+                onChange={(e) => setWine.attributes.finish(e.target.value)}>
+              </input>
+              <label htmlFor='tannins' className='addWineLabel'>Tannins:</label>
+              <input
+                type="text"
+                name="type"
+                className="addWineInput"
+                value={wine.attributes.tannins}
+                onChange={(e) => setWine.attributes.tannins(e.target.value)}>
+              </input>
+              <label htmlFor='fruitlevel' className='addWineLabel'>Fruit Level:</label>
+              <input
+                type="text"
+                name="type"
+                className="addWineInput"
+                value={wine.attributes.fruitLevel}
+                onChange={(e) => setWine.attributes.fruitLevel(e.target.value)}>
+              </input>
+              <br>
+              </br>
+              <label htmlFor='variety' className='addWineLabel'>Variety:</label>
+              <input
+                type="text"
+                name="type"
+                className="addWineInput"
+                value={wine.variety}
+                onChange={(e) => setWine.variety(e.target.value)}>
+              </input>
+              <label htmlFor='age' className='addWineLabel'>Age:</label>
+              <input
+                type="text"
+                name="type"
+                className="addWineInput"
+                value={wine.region}
+                onChange={(e) => setWine.region(e.target.value)}>
+              </input>
+              <button type="submit" className='addWineButton'>Add Wine</button>
+            </form>
+          </h1>
+        </div>
+      </div>
     </div>
   )
 }
