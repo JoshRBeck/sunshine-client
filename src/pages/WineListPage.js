a757e154413045a22c2989e10516ee30e75e36f9
Lukas Jülich <lukas-juelich@web.de>
2023-05-16 14:37:09 +0200

    added new wine image

--- src/pages/WineListPage.js
+++ src/pages/WineListPage.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import WhiteWine from "../assets/white-wine.png";
 import RedWine from "../assets/red-wine.png";
-import RoseWine from "../assets/rose-wine.png";
+import Rose3 from "../assets/Rose3.png"
 import location from "../assets/location.png"
 import grapes from "../assets/grapes.png"
 import cards from "../assets/cards.png"
@@ -65,7 +65,7 @@ function WineListPage(props) {
                 <img className="Wine" src={RedWine} alt="Red Wine" />
               )}
               {wine.type === "Rosé" && (
-                <img className="Wine" src={RoseWine} alt="Rosé Wine" />
+                <img className="Wine" src={Rose3} alt="Rosé Wine" />
               )}
             </div>
           </div>
